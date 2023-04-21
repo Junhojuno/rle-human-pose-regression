2769e254a5bdabf6914639f6570ece60f655c873
Junhojuno <rlawnsgh2245@gmail.com>
2023-04-21 21:29:08 +0900

    feat: ghostnetv2 추가, efficientnet load_weights 수정

--- src/backbone/__init__.py
+++ src/backbone/__init__.py
@@ -1,10 +1,10 @@
 from typing import List, Optional
-import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras import layers
 from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-from tensorflow.keras.applications.efficientnet import EfficientNetB0
+# from tensorflow.keras.applications.efficientnet import EfficientNetB0 as EfficientNetB0TF
+from src.backbone.efficientnet import EfficientNetB0
+from src.backbone.ghostnetv2 import GhostNetV2
 
 from src.backbone.resnet_pf import ResNet50Hybrid
 
@@ -19,8 +19,15 @@ def build_backbone(
         backbone = MobileNetV2(include_top=False, input_shape=input_shape)
     elif backbone_type == 'resnet50-pf':
         backbone = ResNet50Hybrid(include_top=False, input_shape=input_shape)
-    elif backbone == 'efficientnet':
+    elif backbone_type == 'efficientnet':
         backbone = EfficientNetB0(include_top=False, input_shape=input_shape)
+        # backbone = EfficientNetB0TF(include_top=False, input_shape=input_shape)
+    elif backbone_type == 'ghostnetv2':
+        backbone = GhostNetV2(
+            include_top=False,
+            input_shape=input_shape,
+            width_multiplier=1.0,
+        )
     else:
         raise ValueError(f'{backbone_type} is wrong. check backbone_type')
     return backbone
