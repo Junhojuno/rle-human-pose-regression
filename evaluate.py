49500bc8c75633091fd4bdcf2a00a3513c398132
Junhojuno <rlawnsgh2245@gmail.com>
2023-05-17 16:57:15 +0900

    feat: Update evaluate.py cuz eval modules are changed

--- evaluate.py
+++ evaluate.py
@@ -1,38 +1,96 @@
 from pathlib import Path
+import argparse
 
-from src.model import RLEModel
-from src.eval.coco import load_coco_eval_dataset, eval_coco
-from src.eval.utils import print_name_value
+from src.model import PoseRegModel
+from src.function import validate
+from src.eval import load_eval_dataset
+
+
+def define_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_height', '-ih',
+        dest='input_height',
+        default=256,
+        type=int,
+        required=False
+    )
+    parser.add_argument(
+        '--input_width', '-iw',
+        dest='input_width',
+        default=192,
+        type=int,
+        required=False
+    )
+    parser.add_argument(
+        '--num_keypoints', '-k',
+        dest='num_keypoints',
+        default=17,
+        type=int,
+        required=False
+    )
+    parser.add_argument(
+        '--backbone', '-b',
+        dest='backbone',
+        default='resnet50',
+        type=str,
+        required=False
+    )
+    parser.add_argument(
+        '--weights', '-w',
+        dest='weights',
+        required=True
+    )
+    parser.add_argument(
+        '--flip',
+        dest='use_flip',
+        action='store_true'
+    )
+    return parser.parse_args()
 
 
 def main():
+    args = define_argparser()
     cwd = Path('.').resolve()
 
-    input_shape = [256, 192, 3]
-    num_keypoints = 17
-    weight_path = 'results/only_coco/basic_coco/rle/resnet50/b32x1_lr0.001_s2.0_sf0.25_r45/ckpt/best_model.tf'
+    input_shape = [args.input_height, args.input_width, 3]
+    num_keypoints = args.num_keypoints
+    backbone = args.backbone
+    weight_path = args.weights
 
-    model = RLEModel(
+    model = PoseRegModel(
         num_keypoints,
         input_shape,
-        'resnet50',
-        is_training=True
+        backbone
     )
     model.load_weights(weight_path)
 
-    eval_ds = load_coco_eval_dataset(
-        str(cwd.parent / 'datasets'),
-        input_shape,
+    eval_ds = load_eval_dataset(
+        str(
+            cwd.parent
+            / 'datasets'
+            / 'mscoco'
+            / 'tfrecords/val/*.tfrecord'
+        ),
+        32,
         num_keypoints,
-        batch_size=32
+        input_shape
     )
-    ap, details = eval_coco(
-        eval_ds,
+    _, _ = validate(
         model,
+        eval_ds,
         input_shape,
-        use_flip=True,
+        str(
+            cwd.parent
+            / 'datasets'
+            / 'mscoco'
+            / 'annotations'
+            / 'person_keypoints_val2017.json'
+        ),
+        f'{args.input_height}x{args.input_width}_{backbone}',
+        print,
+        use_flip=args.use_flip
     )
-    print_name_value(details, 'rle_model', print_fn=print)
 
 
 if __name__ == '__main__':
